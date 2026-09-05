7710ef41f4acbfebcb2c0bb8f6950693bca1bf7b
m9kllllll <71499564+m9kllllll@users.noreply.github.com>
2026-09-05 11:04:51 +0800

    Export tracing and evaluation helpers

--- rag_upgrades/__init__.py
+++ rag_upgrades/__init__.py
@@ -3,3 +3,5 @@ from .intelligent_chunking import IntelligentChunker, intelligent_chunk_document
 from .contextual_metadata import enrich_document_metadata
 from .image_captioning import ImageCaptioner
 from .rrf import reciprocal_rank_fusion
+from .tracing import WhiteBoxTracer, traced_span
+from .evaluation import RAGEvaluator, evaluate_retrieval, compare_reports
